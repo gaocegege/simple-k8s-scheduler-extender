cf4dec9d3f9724830595ebdc5375251aeefd8440
zjx <271540124@qq.com>
2016-09-22 11:41:12 +0800

    get node resource status through api

--- algorithm/prioritize.go
+++ algorithm/prioritize.go
@@ -3,6 +3,8 @@ package algorithm
 import (
 	"k8s.io/kubernetes/pkg/api"
 	schedulerapi "k8s.io/kubernetes/plugin/pkg/scheduler/api"
+	"../nodeStatus"
+	"fmt"
 )
 
 // LeastHostedPriority is a priority function that favors nodes with less hosts.
@@ -11,24 +13,64 @@ func LeastHostedPriority(args *schedulerapi.ExtenderArgs) schedulerapi.HostPrior
 
 	pod := args.Pod
 	nodes := args.Nodes
+	pods := &nodeStatus.GetAllPods()
 
 	for _, node := range nodes.Items {
-		result = append(result, calculateResourceScore(&pod, &node))
+		ePods := nodeStatus.GetPodsByNodeName(pods, node.Name)
+		result = append(result, calculateResourceScore(&pod, &node, ePods))
 	}
 	return result
 }
 
-func calculateResourceScore(pod *api.Pod, node *api.Node) schedulerapi.HostPriority {
+func calculateResourceScore(pod *api.Pod, node *api.Node, epods []*api.Pod) schedulerapi.HostPriority {
+
 	allocatableMilliCPU := node.Status.Allocatable.Cpu().MilliValue()
 	allocatableMemory := node.Status.Allocatable.Memory().Value()
-	capacityMilliCPU := node.Status.Allocatable.Cpu().MilliValue()
-	capacityMemory := node.Status.Allocatable.Memory().Value()
+	milliCPURequested := int64(0)
+	memoryRequested := int64(0)
+	for _, pod := range epods {
+		fmt.Println("podName: ", pod.Name, "  NodeName: ", pod.Spec.NodeName)
+		fmt.Println("Resource: ")
+		podRequest := getResourceRequest(pod)
+		milliCPURequested += podRequest.milliCPU
+		fmt.Println("  cpu: ", podRequest.milliCPU)
+		memoryRequested += podRequest.memory
+		fmt.Println("  memory: ", podRequest.memory)
+	}
+
+	canuseMilliCPU := allocatableMilliCPU - milliCPURequested
+	canuseMemory := allocatableMemory - memoryRequested
+	fmt.Println("Node: ", node.Name)
+	fmt.Println("allocatable: ")
+	fmt.Println("    cpu: ", allocatableMilliCPU)
+	fmt.Println("    memory: ", allocatableMemory)
+	fmt.Println("canuse: ")
+	fmt.Println("    cpu: ", canuseMilliCPU)
+	fmt.Println("    memory: ", canuseMemory)
+
+	//capacityMilliCPU := node.Status.Allocatable.Cpu().MilliValue()
+	//capacityMemory := node.Status.Allocatable.Memory().Value()
 
 	return schedulerapi.HostPriority{
 		Host: node.Name,
-		Score: (calculateScore(allocatableMilliCPU, capacityMilliCPU) +
-			calculateScore(allocatableMemory, capacityMemory)) / 2,
+		Score: (calculateScore(canuseMilliCPU, allocatableMilliCPU) +
+			calculateScore(canuseMemory, allocatableMemory)) / 2,
 	}
+}
+
+type resourceRequest struct {
+	milliCPU int64
+	memory   int64
+}
+
+func getResourceRequest(pod *api.Pod) resourceRequest {
+	result := resourceRequest{}
+	for _, container := range pod.Spec.Containers {
+		requests := container.Resources.Requests
+		result.memory += requests.Memory().Value()
+		result.milliCPU += requests.Cpu().MilliValue()
+	}
+	return result
 }
 
 // the unused capacity is calculated on a scale of 0-10
---
+++ nodeStatus/nodeStatus.go
@@ -0,0 +1,82 @@
+package nodeStatus
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"net/http"
+	"fmt"
+	"bytes"
+	"io/ioutil"
+	"encoding/json"
+)
+
+const (
+	userName string = "admin"
+	password string = "FlbY3CD6mcFUfZvb"
+	destinationServer_Caicloud string = "https://sjtu.caicloudapp.com"
+	allpod_caicloud string = "api/v1/pods"
+	destinationServer_Test string = "http://202.120.40.177:16380"
+)
+
+func setBasicAuthOfCaicloud(r *http.Request) {
+	r.SetBasicAuth(userName, password)
+}
+
+func InvokeRequest_Caicloud(method string, url string, body []byte) *http.Response {
+	client := http.Client{}
+	var req *http.Request
+	var err error
+	if body != nil {
+		req, err = http.NewRequest(method, url, bytes.NewBuffer(body))
+	} else {
+		req, err = http.NewRequest(method, url, nil)
+	}
+
+	setBasicAuthOfCaicloud(req)
+	if method != "GET" {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	resp, err := client.Do(req)
+	//fmt.Println(resp.Header)
+	//fmt.Println(resp.Status)
+	//fmt.Println(resp.StatusCode)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return resp
+}
+func InvokeGetReuqest(url string) *http.Response {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err)
+		return nil
+	}
+	return resp
+}
+
+func GetAllPods() api.PodList {
+	//resp := InvokeRequest_Caicloud("GET", destinationServer_Caicloud + allpod_caicloud, nil)
+	resp := InvokeGetReuqest(destinationServer_Test + allpod_caicloud)
+	var v api.PodList
+
+	if (resp != nil) {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if (err != nil) {
+			fmt.Print(err)
+			return v
+		}
+		if err = json.Unmarshal(body, &v); err != nil {
+		}
+		return v
+	}
+	return v
+}
+func GetPodsByNodeName(podList *api.PodList, nodeName string) []*api.Pod {
+	var pods []*api.Pod
+	for _, pod := range podList.Items {
+		if pod.Spec.NodeName == nodeName {
+			pods = append(pods, &pod)
+		}
+	}
+	return pods
+}
